5755b0df05634422d46678d11047f0ad8e662c3d
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2019-11-26 20:13:49 -0300

    Ver. 0.01

---
+++ SeuTCC/app/Http/Controllers/TccController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tcc;
+use Illuminate\Http\Request;
+
+class TccController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tcc  $tcc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tcc  $tcc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tcc  $tcc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tcc  $tcc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tcc $tcc)
+    {
+        //
+    }
+}
---
+++ SeuTCC/database/factories/TccFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tcc;
+use Faker\Generator as Faker;
+
+$factory->define(Tcc::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
