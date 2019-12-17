c54062a07c26a1d2e8736a4ea82684052b8021e4
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2019-12-17 03:23:41 -0300

    final

--- MeuTCC/routes/web.php
+++ MeuTCC/routes/web.php
@@ -18,30 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/sobre', ['as' => 'about', 'uses' => 'HomeController@sobre']);
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
-
-
-
-Route::get('/ofertaTcc',['as'=>'professor.ofertaTcc', 'uses'=>'ProfessorController@index']);
-Route::post('/ofertaTcc',['as'=>'professor.ofertaTcc.submit', 'uses'=>'ProfessorController@createOffer']);
-
-Route::get('/tcc/schedule/add',['as'=> 'tcc.schedule.add', 'uses' =>'TccController@add']);
-Route::put('/tcc/schedule/salvar',['as'=> 'tcc.schedule.salvar', 'uses' =>'TccController@salvar']);
-
-Route::get('/tcc/schedule/show',['as'=> 'tcc.schedule.show', 'uses' =>'TccController@show']);
-Route::get('/tcc/schedule/view',['as'=> 'tcc.schedule.view', 'uses' =>'TccController@view']);
-Route::get('/tcc/schedule/editar',['as'=> 'tcc.schedule.editar' ,'uses' =>'TccController@editar']);
-Route::post('/tcc/schedule/atualizar',['as'=> 'tcc.schedule.atualizar', 'uses' =>'TccController@atualizar', ]);
-
-
-Route::get('/tcc',['as'=>'tcc', 'uses'=>'TccController@index']);
-Route::get('/tcc/cadastrar', ['as'=>'tcc.create', 'uses'=>'TccController@create']);
-Route::post('/tcc/salvar', ['as'=>'tcc.salvar', 'uses'=>'TccController@save']);
-Route::get('/alunos',['as'=>'alunos', 'uses'=>'PageController@aluno']);
-Route::get('/professores',['as'=>'professores', 'uses'=>'PageController@professor']);
-Route::get('/criarTcc/{id}',['as'=>'criar.tcc', 'uses'=>'TccController@criar']);	
-Route::post('/criarTcc/{id}',['as'=>'tccs.inscricao', 'uses'=>'TccController@inscricao']);
 
 Route::group(['middleware' => 'auth'], function () {
 		Route::get('icons', ['as' => 'pages.icons', 'uses' => 'PageController@icons']);
@@ -50,6 +27,28 @@ Route::group(['middleware' => 'auth'], function () {
 		Route::get('tables', ['as' => 'pages.tables', 'uses' => 'PageController@tables']);
 		Route::get('typography', ['as' => 'pages.typography', 'uses' => 'PageController@typography']);
 		Route::get('upgrade', ['as' => 'pages.upgrade', 'uses' => 'PageController@upgrade']);
+
+		Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+		Route::get('/ofertaTcc',['as'=>'professor.ofertaTcc', 'uses'=>'ProfessorController@index']);
+		Route::post('/ofertaTcc',['as'=>'professor.ofertaTcc.submit', 'uses'=>'ProfessorController@createOffer']);
+
+		Route::get('/tcc/schedule/add',['as'=> 'tcc.schedule.add', 'uses' =>'TccController@add']);
+		Route::put('/tcc/schedule/salvar',['as'=> 'tcc.schedule.salvar', 'uses' =>'TccController@salvar']);
+
+		Route::get('/tcc/schedule/show',['as'=> 'tcc.schedule.show', 'uses' =>'TccController@show']);
+		Route::get('/tcc/schedule/view',['as'=> 'tcc.schedule.view', 'uses' =>'TccController@view']);
+		Route::get('/tcc/schedule/editar',['as'=> 'tcc.schedule.editar' ,'uses' =>'TccController@editar']);
+		Route::post('/tcc/schedule/atualizar',['as'=> 'tcc.schedule.atualizar', 'uses' =>'TccController@atualizar', ]);
+
+		Route::get('/tcc',['as'=>'tcc', 'uses'=>'TccController@index']);
+		Route::get('/tcc/cadastrar', ['as'=>'tcc.create', 'uses'=>'TccController@create']);
+		Route::post('/tcc/salvar', ['as'=>'tcc.salvar', 'uses'=>'TccController@save']);
+		Route::get('/alunos',['as'=>'alunos', 'uses'=>'PageController@aluno']);
+		Route::get('/professores',['as'=>'professores', 'uses'=>'PageController@professor']);
+		Route::get('/criarTcc/{id}',['as'=>'criar.tcc', 'uses'=>'TccController@criar']);	
+		Route::post('/criarTcc/{id}',['as'=>'tccs.inscricao', 'uses'=>'TccController@inscricao']);
+
 });
 
 Route::group(['middleware' => 'auth'], function () {
